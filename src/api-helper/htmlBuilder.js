b6098411ce860c9d5405112956bed59557f642e3
Dionei J. Delangolo <dionei.jd@gmail.com>
2021-08-26 21:55:26 -0300

    Improved helpers

---
+++ src/api-helper/htmlBuilder.js
@@ -0,0 +1,81 @@
+const lightGray  = '#ffffff',
+      lightGreen = '#66cc91',
+      softWhite  = '#f0f0f0',
+      white      = '#ffffff'
+
+
+
+const builder = {
+
+    SetPageHeader: (title, subTitle) => {
+        const style = `
+                    background: ${softWhite};
+                    position: fixed;
+                    padding-left: 10px;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    border-bottom: 2px solid ${lightGreen};
+                    box-shadow: 0px 1px 5px ${lightGreen};
+                    `
+
+        return `<div style="${style}">
+                    <h1 style="font-family: sans-serif; font-size: 38px; margin-bottom: 0px">${title}</h1>
+                    <h3 style="font-family: sans-serif; margin: 0 0 10px 0; color: ${lightGreen}" >${subTitle}</h3>
+                </div>
+                <div style="height: 120px; background: ${white};"></div>`
+
+    },
+    SetBoxTitle: (children) => {
+        const style = `
+                    background: ${lightGreen}; 
+                    padding: 10px;
+                    margin: 0px;
+                    font-family: sans-serif;
+                    `
+
+        return `<div style="${style}">
+                    ${children}
+                </div>`
+    },
+
+    SetBoxBody: (children) => {
+        const style = `
+                    background: ${white}; 
+                    padding: 10px;
+                    margin: 0px;
+                    border: solid 1px ${lightGreen}; 
+                    border-top-style: none;
+                    font-family: sans-serif;
+                    `
+
+        return `<div style="${style}">
+                    ${children}
+                </div>`
+    },
+
+    ApiJsonReturn: (jsonStr) => {
+        const json = JSON.stringify(JSON.parse(jsonStr),null,4)
+        const rows = (json.match(new RegExp('\n', 'g')) || []).length + 2
+
+        const styleDiv = `
+                    background: ${softWhite}; 
+                    padding: 20px;
+                    margin-top: 5px;
+                    `
+        
+        const styleTxt = `
+                    background: transparent;
+                    border: none;
+                    resize: none;
+                    font-family: sans-serif;
+                    `
+
+        return `<div style="${styleDiv}">
+                    <textarea style="${styleTxt}" readonly rows=${rows} cols="100%">${json}
+                    </textarea>
+                </div>`
+    }
+}
+
+module.exports = builder
--- src/routes.js
+++ src/routes.js
@@ -1,115 +1,51 @@
 const express = require('express')
 const routes = express.Router()
+const b = require('./api-helper/htmlBuilder')
+const sf = require('./api-helper/docs-sf')
 
-routes.get('/', (req, res) =>{
 
-    let html, temp
-    html = ''
-    
-    html += '<h1 style="font-family: sans-serif; font-size: 38px">My APIs</h1>'
-    html += '<hr style="border: 1px solid #66cc91"/>'
-    html += '<h3>SECRECT FRIEND</h3>'
 
-    html += SetBoxTitle('<b>GROUPS - GET (list)</b>')
-    html += SetBoxBody('<b>Url:</b> /sf/groups')
-    temp  = '<b>Return 200:</b>'
-    temp += ApiJsonReturn('["6015ed2ea9d63d26d0450ea5","6125782b3c2dcd0b983fe3c5"]')    
-    html += SetBoxBody(temp)    
-
-    html += '<br/><br/><br/>'    
-
-    html += SetBoxTitle('<b>GROUPS - GET (by ID)</b>')
-    html += SetBoxBody('<b>Url:</b> /sf/groups/:id')
-    temp  = '<b>Return 200:</b>'
-    temp += ApiJsonReturn('{"_id": "6015ed2ea9d63d26d0450ea5","people": [{"_id": "6015ed2ea9d63d26d0450ea6","name": "Dionei","friend": "Grasi"},{"_id": "6015ed2ea9d63d26d0450ea7","name": "Manu","friend": "Dionei"},{"_id": "6015ed2ea9d63d26d0450ea8","name": "Grasi","friend": "Manu"}],"createdAt": "2021-01-30T23:35:10.528Z","updatedAt": "2021-01-30T23:35:10.528Z","__v": 0}')
-    html += SetBoxBody(temp)
-    temp  = '<b>Return 500:</b>'
-    temp += ApiJsonReturn('{"error": "051","errorMsg": "Group id 5fcc32cc708b471f9cef6a64 not found"}')
-    html += SetBoxBody(temp)
-
-    html += '<br/><br/><br/>'    
-
-    html += SetBoxTitle('<b>GROUPS - POST </b>')
-    html += SetBoxBody('<b>Url:</b> /sf/groups')
-    temp  = '<b>Body Input:</b>'
-    temp += ApiJsonReturn('{"name": "Grupo da Familia","people": [{"name": "Dionei"},{"name": "Manu"},{"name": "Duda"}]}')    
-    html += SetBoxBody(temp)
-    temp  = '<b>Return 201:</b>'
-    temp += ApiJsonReturn('{"_id": "6125782b3c2dcd0b983fe3c5","name": "Grupo da Familia","people": [{"_id": "6125782b3c2dcd0b983fe3c6","name": "Dionei","friend": "Duda"},{"_id": "6125782b3c2dcd0b983fe3c7","name": "Manu","friend": "Dionei"},{"_id": "6125782b3c2dcd0b983fe3c8","name": "Duda","friend": "Manu"}],"createdAt": "2021-08-24T22:52:27.362Z","updatedAt": "2021-08-24T22:52:27.362Z"}')
-    html += SetBoxBody(temp)    
-    temp  = '<b>Return 400:</b>'
-    temp += ApiJsonReturn('{"error": "041", "errorMsg": "Its missing the group name"}')
-    temp += ApiJsonReturn('{"error": "042", "errorMsg": "Its missing the people"}')
-    temp += ApiJsonReturn('{"error": "043", "errorMsg": "It should have at least 3 people to create a group"}')
-    temp += ApiJsonReturn('{"error": "044", "errorMsg": "People node has duplicate names, it not allowed."}')
-    html += SetBoxBody(temp)
-    temp  = '<b>Return 500:</b>'
-    temp += ApiJsonReturn('{"error": "051", "errorMsg": "Error to create group"}')
-    html += SetBoxBody(temp)
-
-    html += '<br/><br/><br/>'    
-
-    html += SetBoxTitle('<b>PERSON - GET (by ID)</b>')
-    html += SetBoxBody('<b>Url:</b> /sf/p/:id')
-    temp  = '<b>Return 200:</b>'
-    temp += ApiJsonReturn('{"_id": "6125782b3c2dcd0b983fe3c7","name": "Manu","friend": "Dionei","grpName": "Grupo da Familia"}')
-    html += SetBoxBody(temp)
-    temp  = '<b>Return 404:</b>'
-    temp += ApiJsonReturn('{"error": "041","errorMsg": "Person id 6015867353c2b20e5cbc2bca not found"}')
-    html += SetBoxBody(temp)
+routes.get('/help/sf/groups', (req, res) => {
+    let html = ''
+    html  = b.SetPageHeader('MY API', 'SECRECT FRIENDS')
+    html += sf.HelpOfGroups()
 
     return res.send(html)
-} )
+})
 
-function SetBoxTitle(children){
-    const style = `
-                 background: #66cc91; 
-                 padding: 10px;
-                 margin: 0px;
-                 font-family: sans-serif;
-                `
+routes.get('/help/sf/p', (req, res) => {
+    let html = ''
+    html  = b.SetPageHeader('MY API', 'SECRECT FRIENDS')
+    html += sf.HelpOfPerson()
 
-    return `<div style="${style}">
-                ${children}
-            </div>`
-}
+    return res.send(html)
+})
 
-function SetBoxBody(children){
-    const style = `
-                 background: #ffffff; 
-                 padding: 10px;
-                 margin: 0px;
-                 border: solid 1px #66cc91;
-                 font-family: sans-serif;
-                `
+routes.get('/help/sf', (req, res) => {
+    let html = ''
+    html  = b.SetPageHeader('MY API', 'SECRECT FRIENDS')
+    html += sf.HelpOfGroups()
+    html += '<br/><br/><br/>'    
+    html += sf.HelpOfPerson()
 
-    return `<div style="${style}">
-                ${children}
-            </div>`
+    return res.send(html)
+})
 
-}
-
-function ApiJsonReturn(jsonStr){
-    const json = JSON.stringify(JSON.parse(jsonStr),null,4)
-    const rows = (json.match(new RegExp('\n', 'g')) || []).length + 2
-
-    const styleDiv = `
-                 background: #f0f0f0; 
-                 padding: 20px;
-                 margin-top: 5px;
-                `
-    
-    const styleTxt = `
-                 background: transparent;
-                 border: none;
-                 resize: none;
-                 font-family: sans-serif;
-                `
-
-    return `<div style="${styleDiv}">
-                <textarea style="${styleTxt}" readonly rows=${rows} cols="100%">${json}
-                </textarea>
-            </div>`
-}
+// Redirecting
+routes.get('/', (req, res) =>{
+    return res.redirect('/help/sf')
+})
+routes.get('/help', (req, res) =>{
+    return res.redirect('/help/sf')
+})
+routes.get('/help/sf/person', (req, res) => {
+    return res.redirect('/help/sf/p')
+})
+routes.get('/help/sf/people', (req, res) => {
+    return res.redirect('/help/sf/p')
+})
+routes.get('/help/sf/group', (req, res) => {
+    return res.redirect('/help/sf/groups')
+})
 
 module.exports = routes
